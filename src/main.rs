3b027e28c1bc53d60e9e63f0508f9a67cb9e7bc4
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2021-04-27 10:15:09 +0200

    Refactor various parts

--- src/main.rs
+++ src/main.rs
@@ -77,65 +77,89 @@ impl Fische {
         ])
     }
 
+    fn get_blau(&self) -> &Option<Fisch> {
+        &self.0[0]
+    }
+
+    fn get_mut_blau(&mut self) -> &mut Option<Fisch> {
+        &mut self.0[0]
+    }
+
+    fn get_orange(&self) -> &Option<Fisch> {
+        &self.0[1]
+    }
+
+    fn get_mut_orange(&mut self) -> &mut Option<Fisch> {
+        &mut self.0[1]
+    }
+
+    fn get_gelb(&self) -> &Option<Fisch> {
+        &self.0[2]
+    }
+
+    fn get_mut_gelb(&mut self) -> &mut Option<Fisch> {
+        &mut self.0[2]
+    }
+
+    fn get_rosa(&self) -> &Option<Fisch> {
+        &self.0[3]
+    }
+
+    fn get_mut_rosa(&mut self) -> &mut Option<Fisch> {
+        &mut self.0[3]
+    }
+
+    fn get_mut_col(&mut self, col: &Color) -> &mut Option<Fisch> {
+        match col {
+            Color::Blau => self.get_mut_blau(),
+            Color::Orange => self.get_mut_orange(),
+            Color::Gelb => self.get_mut_gelb(),
+            Color::Rosa => self.get_mut_rosa(),
+            _ => panic!("Not a fishcolor"),
+        }
+    }
+
     pub fn has(&self, col: &Color) -> bool {
         match col {
-            Color::Blau => self.0[0],
-            Color::Orange => self.0[1],
-            Color::Gelb => self.0[2],
-            Color::Rosa => self.0[3],
-            _ => None,
+            Color::Blau => self.get_blau(),
+            Color::Orange => self.get_orange(),
+            Color::Gelb => self.get_gelb(),
+            Color::Rosa => self.get_rosa(),
+            _ => &None,
         }
         .is_some()
     }
 
     pub fn get_first(&self) -> &Option<Fisch> {
         if self.has(&Color::Blau) {
-            &self.0[0]
+            self.get_blau()
         } else if self.has(&Color::Orange) {
-            &self.0[1]
+            self.get_orange()
         } else if self.has(&Color::Gelb) {
-            &self.0[2]
+            self.get_gelb()
         } else if self.has(&Color::Rosa) {
-            &self.0[3]
+            self.get_rosa()
         } else {
             &None
         }
     }
 
     pub fn extract_first(&mut self) -> Option<Fisch> {
-        if self.has(&Color::Blau) {
-            self.0[0].take()
-        } else if self.has(&Color::Orange) {
-            self.0[1].take()
-        } else if self.has(&Color::Gelb) {
-            self.0[2].take()
-        } else if self.has(&Color::Rosa) {
-            self.0[3].take()
+        let col = self.get_first().map(|f| f.0);
+
+        if let Some(col) = col {
+            self.get_mut_col(&col).take()
         } else {
             None
         }
     }
 
     pub fn rm(&mut self, col: &Color) {
-        match col {
-            Color::Blau => &mut self.0[0],
-            Color::Orange => &mut self.0[1],
-            Color::Gelb => &mut self.0[2],
-            Color::Rosa => &mut self.0[3],
-            _ => panic!("Not a fishcolor"),
-        }
-        .take();
+        self.get_mut_col(col).take();
     }
 
     pub fn add(&mut self, col: Color) {
-        match col {
-            Color::Blau => &mut self.0[0],
-            Color::Orange => &mut self.0[1],
-            Color::Gelb => &mut self.0[2],
-            Color::Rosa => &mut self.0[3],
-            _ => panic!("Not a fishcolor"),
-        }
-        .replace(Fisch(col));
+        self.get_mut_col(&col).replace(Fisch(col));
     }
 }
 
@@ -158,8 +182,18 @@ impl Game {
                 Fische::default(),
                 Fische::default(),
                 Fische::default(),
-                Fische::fill(),
-                Fische::default(),
+                Fische([
+                    Some(Fisch(Color::Blau)),
+                    Some(Fisch(Color::Orange)),
+                    None,
+                    None,
+                ]),
+                Fische([
+                    None,
+                    None,
+                    Some(Fisch(Color::Gelb)),
+                    Some(Fisch(Color::Rosa)),
+                ]),
                 Fische::default(),
                 Fische::default(),
                 Fische::default(),
@@ -307,53 +341,53 @@ impl Display for Game {
 }
 
 fn main() {
-    manual_game();
-    // benchmark();
+    // manual_game();
+    benchmark();
 }
 
-// fn benchmark() {
-//     let runs = 29718;
-//     let winners = (0..=runs)
-//         .step_by(1)
-//         .map(|_| {
-//             let mut g = Game::new();
-//             while g.tick() == Winner::Undecided {}
-//             g.tick()
-//         })
-//         .collect::<Vec<Winner>>();
+fn benchmark() {
+    let runs = 29718;
+    let winners = (0..=runs)
+        .step_by(1)
+        .map(|_| {
+            let mut g = Game::new();
+            while g.tick() == Winner::Undecided {}
+            g.tick()
+        })
+        .collect::<Vec<Winner>>();
 
-//     let b_win = winners.iter().filter(|w| w == &&Winner::Boot).count();
-//     let f_win = winners.iter().filter(|w| w == &&Winner::Fisch).count();
-//     let tie = winners
-//         .iter()
-//         .filter(|w| w == &&Winner::Unentschieden)
-//         .count();
+    let b_win = winners.iter().filter(|w| w == &&Winner::Boot).count();
+    let f_win = winners.iter().filter(|w| w == &&Winner::Fisch).count();
+    let tie = winners
+        .iter()
+        .filter(|w| w == &&Winner::Unentschieden)
+        .count();
 
-//     println!(
-//         "Boot: {} ({:.2}%)",
-//         b_win,
-//         (b_win as f32).div(runs as f32).mul(100f32)
-//     );
-//     println!(
-//         "Fisch: {} ({:.2}%)",
-//         f_win,
-//         (f_win as f32).div(runs as f32).mul(100f32)
-//     );
-//     println!(
-//         "Unentschieden: {} ({:.2}%)",
-//         tie,
-//         (tie as f32).div(runs as f32).mul(100f32)
-//     );
+    println!(
+        "Boot: {} ({:.2}%)",
+        b_win,
+        (b_win as f32).div(runs as f32).mul(100f32)
+    );
+    println!(
+        "Fisch: {} ({:.2}%)",
+        f_win,
+        (f_win as f32).div(runs as f32).mul(100f32)
+    );
+    println!(
+        "Unentschieden: {} ({:.2}%)",
+        tie,
+        (tie as f32).div(runs as f32).mul(100f32)
+    );
+}
+
+// fn manual_game() {
+//     let mut g = Game::new();
+//     println!("{}", g);
+
+//     while g.tick() == Winner::Undecided {
+//         println!("{}", g);
+//         std::thread::sleep(std::time::Duration::from_millis(250))
+//     }
+
+//     println!("Winner: {:?}", g.tick());
 // }
-
-fn manual_game() {
-    let mut g = Game::new();
-    println!("{}", g);
-
-    while g.tick() == Winner::Undecided {
-        println!("{}", g);
-        std::thread::sleep(std::time::Duration::from_millis(250))
-    }
-
-    println!("Winner: {:?}", g.tick());
-}
